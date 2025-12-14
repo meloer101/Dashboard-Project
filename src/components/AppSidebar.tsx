6a2d4a884ff1cf14aff84b8e1143630cf3fad72c
Jacoy <jacobyu80@gmail.com>
2025-12-14 23:34:16 +0800

    初始化完毕

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -2,6 +2,12 @@ import { cn } from "@/lib/utils";
 
 import { Sidebar,SidebarContent,SidebarFooter,SidebarGroup,SidebarHeader } from "@/components/ui/sidebar";
 import Avatar from "react";
+import {Button} from "@/components/ui/button";
+
+import {useSidebar} from "@/components/ui/sidebar";
+
+import{ LogOutIcon } from 'lucide-react';
+//import {Logo} from "@/components/Logo";
 
 export const AppSidebar = () => {
   return <div>App Sidebar</div>;
