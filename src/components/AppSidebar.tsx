17e7aa75f79ca094a99126e09d314ca35d3cfa4d
Jacoy <jacobyu80@gmail.com>
2025-12-15 18:36:38 +0800

    AppsideBar 初步完成

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -11,8 +11,10 @@ import {
   SidebarGroupContent,
   SidebarMenuButton,
 } from "@/components/ui/sidebar";
-import Avatar, { use, useEffect } from "react";
+import Avatar from "react-avatar";
+import { use, useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { UserMenu } from "@/components/UserMenu";
 
 import { useSidebar } from "@/components/ui/sidebar";
 
@@ -22,10 +24,9 @@ import { Logo } from "@/components/Logo";
 import { APP_SIDEBAR } from "@/constants";
 
 export const AppSidebar = () => {
-  const { isMobile ,toggleSidebar} = useSidebar();
+  const { isMobile, toggleSidebar } = useSidebar();
 
   useEffect(() => toggleSidebar(), [toggleSidebar]);
-
 
   return (
     <Sidebar variant="floating" collapsible="icon">
@@ -46,9 +47,9 @@ export const AppSidebar = () => {
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton tooltip={item.title} asChild>
                     <a href={item.url}>
-                        <item.Icon/>
+                      <item.Icon />
 
-                        <span>{item.title}</span>
+                      <span>{item.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
@@ -58,26 +59,61 @@ export const AppSidebar = () => {
         </SidebarGroup>
 
         {/*Secondary Navigation*/}
-        <SidebarGroup className="mt-auto" >
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {APP_SIDEBAR.secondaryNav.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton tooltip={item.title} asChild>
-                    <a href={item.url}>
-                        <item.Icon/>
+        {isMobile && (
+          <SidebarGroup className="mt-auto">
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {APP_SIDEBAR.secondaryNav.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton tooltip={item.title} asChild>
+                      <a href={item.url}>
+                        <item.Icon />
 
                         <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-
-
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        )}
       </SidebarContent>
+
+      {/* Sidebar Footer */}
+      <SidebarFooter className={cn(isMobile && "border-t")}>
+        <SidebarMenu>
+          <SidebarMenuItem className={cn(isMobile && "p-2")}>
+            {isMobile ? (
+              <div className="flex justify-between items-start gap-2">
+                <div className="grid grid-cols-[max-content_minmax(0,1fr)] items-center gap-2">
+                  <div className="relative">
+                    <Avatar
+                      src={APP_SIDEBAR.curProfile.src}
+                      size="36px"
+                      round="8px"
+                    />
+                    <div className="absolute bottom-0 right-0 size-2 rounded-full bg-emerald-500 dark:bg-emarald-400 ring-sidebar ring-1"></div>
+                  </div>
+                  <div>
+                    <h3 className="text-sm font-semibold">
+                      {APP_SIDEBAR.curProfile.name}
+                    </h3>
+                    <p className="text-sm text-muted-foreground truncate">
+                      {APP_SIDEBAR.curProfile.email}
+                    </p>
+                  </div>
+                </div>
+                <Button variant='ghost' size='icon-sm'>
+                    <LogOutIcon/>
+                </Button>
+              </div>
+            ) : (
+              <UserMenu />
+            )}
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
