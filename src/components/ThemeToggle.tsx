a101968f962d720ecc1b6c7bb9f5fdd4b867ab40
Jacoy <jacobyu80@gmail.com>
2025-12-16 13:58:01 +0800

    除了主体之外的其他所有大体完成

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,7 +4,7 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
+} from "@/components/ui/dropdown-menu";
 
 import { useTheme } from "@/components/ThemeProvider";
 
@@ -17,10 +17,30 @@ export const ThemeToggle = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" aria-label="Toggle theme">
-            <SunIcon className='scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90'/>
-            <MoonIcon className=' absolute scale-0 rotate-90 transition-all dark:scale-100 dark:-rotate-0'/>
+          <SunIcon className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <MoonIcon className=" absolute scale-0 rotate-90 transition-all dark:scale-100 dark:-rotate-0" />
         </Button>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent align="end" className=" w-40">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <SunIcon />
+          <span>Light</span>
+          {theme === "light" && <CheckIcon className="ms-auto" />}
+        </DropdownMenuItem>
+
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <MoonIcon />
+          <span>Dark</span>
+          {theme === "dark" && <CheckIcon className="ms-auto" />}
+        </DropdownMenuItem>
+
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <MonitorIcon />
+          <span>System</span>
+          {theme === "system" && <CheckIcon className="ms-auto" />}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
